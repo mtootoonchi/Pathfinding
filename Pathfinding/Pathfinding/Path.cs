813582fe4599948c487d341d208d78153a184aa7
Matthew Faraz Tootoonchi <44936371+mtootoonchi@users.noreply.github.com>
2019-02-19 14:29:21 -0800

    Add files via upload

---
+++ Pathfinding/Pathfinding/Path.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinding
+{
+    class Path
+    {
+        public List<string> FindPath(string[,] arr)
+        {
+            for (int j = 0; j <= arr.GetLength(1); j++)
+            {
+                List<string> path = new List<string>();
+                if (j == arr.GetLength(1))
+                {
+                    path.Add("");
+                    return path;
+                }
+                if(arr[0, j].Equals("-"))
+                {
+                    path.Add("0" + j);
+                    for (int i=1;i<= arr.GetLength(0);i++)
+                    {
+                        if(arr.GetLength(0) == i)
+                        {
+                            return path;
+                        }
+                        for(int h=j-1;h<=j+1;h++)
+                        {
+                            if(h<0||h>= arr.GetLength(1))
+                            {
+
+                            }
+                            else if(arr[i, h].Equals("-"))
+                            {
+                                path.Add("" + i + h);
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Pathfinding/Pathfinding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Path t = new Path();
+            Console.WriteLine("How many rows you want: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many cols you want: ");
+            int col = Convert.ToInt32(Console.ReadLine());
+            string[,] arr = new string[row, col];
+            for(int i=0;i<row;i++)
+            {
+                for(int j=0;j<col;j++)
+                {
+                    Console.WriteLine("Is the row: " + i + " and col: " + j + " x or -? (Type \"x\" or \"-\" or error)");
+                    arr[i, j] = Console.ReadLine();
+                }
+            }
+            List<string> ans = t.FindPath(arr);
+            for(int i=0;i<ans.Count;i++)
+            {
+                Console.WriteLine(ans.ElementAt(i) + '\n');
+            }
+        }
+    }
+}
